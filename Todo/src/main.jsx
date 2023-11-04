237b6364afd80bf13690adcc44a48cfd4f3af263
Shemsou <shemsoudev@gmail.com>
2023-11-04 14:08:48 +0100

    girding

--- Todo/src/main.jsx
+++ Todo/src/main.jsx
@@ -3,42 +3,43 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import Tab from './Tab1.jsx'; // Correct the file name
 import Box from './Box.jsx'; // Correct the file name
+import './index.css'
 import { Grid, Paper } from '@mui/material';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
-<Grid container style={{ height: '100vh' }}>
-      <Grid item xs={12}>
-        {/* Row 1: Height is determined by content */}
-        <Paper style={{ height: '50px' }}>
-          {/* Content for row 1 */}
-          Account 
-        </Paper>
-      </Grid>
+<section>
+        <div className="layout text-2xl text-white">
+          <div className="content1 centered">
+           Content1  
+          </div>
+          <div className="content2 centered">
+            Content2  
+          </div>
+          <div className="content3 centered">
+            Content3  
+          </div>
 
-      <Grid item xs={8}>
-        {/* Row 2: Height is determined by content */}
-        <Paper style={{ height: '200px' }}>
-          Timer
-        </Paper>
-      </Grid>
+          <div className="content4 centered">
+            Content4  
+          </div>
+          <div className="content5 centered">
+            Content5  
+          </div>
+          <div className="content6 centered">
+            Content6  
+          </div>
 
-      <Grid item xs={12}>
-        {/* Row 2: Height is determined by content */}
-        <Paper style={{ height: '200px' }}>
-          <App />
-        </Paper>
-      </Grid>
-
-
-
-
-
-    </Grid>
-
-
+          <div className="content7 centered">
+            Content7  
+          </div>
+          <div className="content8 centered">
+            Content8  
+          </div>
+        </div>
+      </section>
 
   </React.StrictMode>
 );
