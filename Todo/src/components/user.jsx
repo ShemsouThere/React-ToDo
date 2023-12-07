c864b9e3a4694c9b8cdf61dac39d36e198a31a17
happycamper2023 <shemsoudev@gmail.com>
2023-12-07 11:57:48 +0100

    google authentification
    only sign-in is working and receiving the JWT token .
    decoding the JWT token isn't working because of a problem in importing JWT-decode library

--- Todo/src/components/user.jsx
+++ Todo/src/components/user.jsx
@@ -1,9 +1,14 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { IoClose } from "react-icons/io5";
 import Cookies from 'js-cookie'; // Import the js-cookie library
+import jwt_decode from "jwt-decode";
 import './user.css';
+
+
+
+
 
 // eslint-disable-next-line react/prop-types
 const CloseButton = ({ onClick }) => {
@@ -107,6 +112,32 @@ export const Loginuser = () => {
   const [password, setPassword] = useState('');
   const [isVisible, setIsVisible] = useState(false);
 
+    useEffect(() => {
+      const handleCallbackResponse = (response) => {
+        console.log("Encoded JWT ID token: " + response.credential);
+        var userObject = jwt_decode(response.credential);
+        console.log(userObject);
+      };
+    
+      /*global google */
+      google.accounts.id.initialize({
+        client_id:"686018352391-stqd9b9o5mpgevcchdmfphd8hf9k6olg.apps.googleusercontent.com", // Replace with your client ID
+        callback: handleCallbackResponse,
+      });
+    
+      const options = {
+        theme: "outline",
+        size: "large",
+        // Other options as needed
+      };
+    
+      const signInDiv = document.getElementById("signInDiv"); // Replace with your element ID
+      if (signInDiv) {
+        google.accounts.id.renderButton(signInDiv, options);
+      }
+    }, []);
+
+
   const handleToggle = () => {
     setIsVisible(!isVisible);
   };
@@ -128,7 +159,7 @@ export const Loginuser = () => {
       .then((response) => {
         console.log('Response:', response.data); // Log the entire response data
         const token = response.data.token;
-        
+  
         // Set the token in cookies
         Cookies.set('jwtToken', token, { expires: 7 }); // 'jwtToken' is the name of the cookie
         console.log('Token:', token); // Log the token to the console
@@ -137,16 +168,35 @@ export const Loginuser = () => {
       })
       .catch((error) => {
         console.error('Login failed:', error);
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          console.error('Error Response Data:', error.response.data);
+          console.error('Error Response Status:', error.response.status);
+          console.error('Error Response Headers:', error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.error('No Response Received:', error.request);
+        } else {
+          // Something happened in setting up the request that triggered an error
+          console.error('Error Setting up Request:', error.message);
+        } 
         alert('Login failed. Please try again.');
       });
   };
   
+  // eslint-disable-next-line no-unused-vars
+  const handleOLogin = () => {
 
+  
+
+  };
 
 
   return (
+    
     <>
       <button onClick={handleToggle}>Login</button>
+      <div id="signInDiv"></div>
       {isVisible && (
         <form className='create_user_form_container'>
           <div className='close-button' onClick={handleClose}>
@@ -172,8 +222,10 @@ export const Loginuser = () => {
           />
           <button className='login-button' onClick={handleLogin}>Login</button>
         </form>
+        
       )}
     </>
   );
+
 };
 
